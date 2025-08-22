9b18296b99a959cd7ddcfc20e4146bb04e1e1f7d
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-08-22 21:21:12 +0800

    student aside done

--- app/Livewire/StudentAside.php
+++ app/Livewire/StudentAside.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Feed;
 use Livewire\Component;
 
 class StudentAside extends Component
 {
+    public $listeners = ["refresh" => '$refresh'];
     public function render()
     {
-        return view('livewire.student-aside');
+        $feeds = Feed::where('group', 'student')->orderByDesc('created_at')->limit(10)->get();
+        return view('livewire.student-aside', compact('feeds'));
     }
 }
