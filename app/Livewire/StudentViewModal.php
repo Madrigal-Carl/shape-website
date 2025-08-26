c8821fdd7788083bd38158c05e195e187fe654a5
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-08-26 09:24:47 +0800

    student view button click

--- app/Livewire/StudentViewModal.php
+++ app/Livewire/StudentViewModal.php
@@ -3,9 +3,26 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class StudentViewModal extends Component
 {
+    public $isOpen = false;
+    public $student_id = null;
+
+    #[On('openModal')]
+    public function openModal($id)
+    {
+        $this->student_id = $id;
+        $this->isOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->isOpen = false;
+        $this->student_id = null;
+    }
+
     public function render()
     {
         return view('livewire.student-view-modal');
