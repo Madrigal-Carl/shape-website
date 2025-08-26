ae327cd1536a1c7a59ef6851e2fd9566dc607258
GeroleoErickson <ericksongeroleo@gmail.com>
2025-08-26 13:35:07 +0800

    add lesson view

--- resources/views/livewire/lesson-main.blade.php
+++ resources/views/livewire/lesson-main.blade.php
@@ -176,4 +176,187 @@
 
     <livewire:lesson-add-modal />
     <livewire:lesson-edit-modal />
+
+    <section class="bg-black/30 absolute w-dvw h-dvh p-10 top-0 left-0 z-50 backdrop-blur-xs flex justify-center gap-6">
+        <!-- Lessson View Info-->
+        <div class="w-150 h-full Addlesson bg-white py-8 rounded-4xl relative flex">
+            <div class="Addlesson w-full h-full flex flex-col pr-8 pl-8 pb-18 gap-8 self-center-safe overflow-y-auto">
+                <div class="flex items-center gap-4">
+                    <img src="src/images/cube.png" alt="">
+                    <h1 class=" text-2xl font-semibold text-heading-dark">Lesson name</h1>
+                </div>
+                
+
+                <div class="flex w-full gap-4">
+                    <div class="bg-gradient-to-tr from-blue-button to-[#00EEFF] shadow-blue-button shadow-2xl/45 p-6 text-white rounded-3xl flex flex-col justify-between gap-6">
+                        <div class="flex justify-between w-45">
+                            <div>
+                                <p class="text-xs leading-snug font-normal">No. of Students</p>
+                                <h1 class="text-lg font-semibold leading-6">Assigned</h1>
+                            </div>
+                            <span class="material-symbols-rounded icon">people</span>
+                        </div>
+                        <h1 class="text-4xl font-semibold">24</h1>
+                    </div>
+
+                    <div class="border-1 border-gray-300 p-6 rounded-3xl w-full flex flex-col gap-2">
+                        <div class="flex items-center w-auto">
+                            <h3 class="text-sm font-semibold w-30">Status:</h3>
+                            <div class="px-2 py-1 rounded-lg bg-[#D2FBD0] w-fit">
+                                <select name="" id="" class="w-fit outline-none text-[#0D5F07] text-sm" >
+                                    <option value="pending" class="text-sm text-black" selected>Active</option>
+                                    <option value="pending" class="text-sm text-black">inactive</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="flex items-center w-auto">
+                            <h3 class="text-sm font-semibold w-30">Subject:</h3>
+                            <p class="text-sm">28143129</p>
+                        </div>
+                        
+                        <div class="flex items-center w-auto">
+                            <h3 class="text-sm font-semibold w-30">Curriculum:</h3>
+                            <p class="text-sm">Hearing, Speech</p>
+                        </div>
+
+                        <div class="flex items-center w-auto">
+                            <h3 class="text-sm font-semibold w-30">No. of Videos:</h3>
+                            <p class="text-sm">99</p>
+                        </div>
+
+                        <div class="flex items-center w-auto">
+                            <h3 class="text-sm font-semibold w-30">No. of Games:</h3>
+                            <p class="text-sm">99</p>
+                        </div>
+
+                        <div class="flex items-center w-auto">
+                            <h3 class="text-sm font-semibold w-30">No. of Quizzes:</h3>
+                            <p class="text-sm">99</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <h1 class="text-lg font-medium">Description</h1>
+                    <p class="text-sm text-paragraph text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in sed sunt. Consectetur ducimus inventore voluptatum, laudantium qui, nihil autem rem excepturi minima reiciendis non alias tempora voluptatem deleniti provident cupiditate magnam. Reiciendis veritatis eum libero perferendis beatae fugiat quod tempore est, aperiam ab placeat debitis accusantium dolore nihil cum.</p>
+                </div>
+
+                <div class="flex flex-col gap-3">
+                    <h2 class="font-medium text-lg">Interactive Video Lessons</h2>
+                    <div class="flex flex-col gap-2">
+
+                        <!-- Default when no video is added -->
+                        <div class="bg-card w-full h-30 hidden items-center justify-center rounded-lg">
+                            <h1 class="text-paragraph">No Video added</h1>
+                        </div>
+
+                        <!-- Video Container -->
+                        <div class="flex grid-cols-2 gap-2">
+                            <div class="flex flex-col gap-2 relative"><!-- Video Holder -->
+                                <div class="flex flex-col items-center justify-center">
+                                    <img src="src/images/video_posters/bighero6.jpeg" alt="" class="aspect-video w-full h-fit rounded-lg object-cover">
+                                    <button class="absolute rounded-full cursor-pointer hover:scale-120 shadow-xl/40 z-10">
+                                        <span class="material-symbols-rounded p-2 rounded-full  playBtn text-white bg-white/20 backdrop-blur-[3px] shadow-white shadow-inner">play_arrow</span>
+                                    </button>
+                                </div>
+                                
+                                <div class="absolute bottom-0 bg-gradient-to-t from-black/80 via-black/0 to-black/0 w-full h-full rounded-lg">
+                                    <div class="h-full w-full flex items-end justify-between p-3">
+                                        <h1 class="text-white font-medium text-sm ml-1">Big Hero 6</h1>
+                                    </div>
+                                </div>
+                            </div><!-- End of Video Holder -->
+
+                            <div class="flex flex-col gap-2 relative"><!-- Video Holder -->
+                                <div class="flex flex-col items-center justify-center">
+                                    <img src="src/images/video_posters/httyd3.jpeg" alt="" class="aspect-video w-full h-fit rounded-lg object-cover">
+                                    <button class="absolute rounded-full cursor-pointer hover:scale-120 shadow-xl/40 z-10">
+                                        <span class="material-symbols-rounded p-2 rounded-full  playBtn text-white bg-white/20 backdrop-blur-[3px] shadow-white shadow-inner">play_arrow</span>
+                                    </button>
+                                </div>
+                                
+                                <div class="absolute bottom-0 bg-gradient-to-t from-black/80 via-black/0 to-black/0 w-full h-full rounded-lg">
+                                    <div class="h-full w-full flex items-end justify-between p-3">
+                                        <h1 class="text-white font-medium text-sm ml-1">How to train your Dragon 3</h1>>
+                                        
+                                    </div>
+                                </div>
+                            </div><!-- End of Video Holder -->
+                        </div><!-- End of Video Container -->
+                    </div>
+                    
+                    
+                </div>
+
+
+                <!-- Games -->
+                <div class="flex flex-col gap-3">
+                    <h2 class="font-medium text-lg">Games & Activities</h2>
+
+                    <!-- Default when no game is added -->
+                    <div class="bg-card w-full h-30 hidden items-center justify-center rounded-lg">
+                        <h1 class="text-paragraph">No Game added</h1>
+                    </div>
+                    
+                    <!-- Game container -->
+                    <div class="w-full grid grid-cols-2 gap-2 items-center justify-center rounded-lg">
+                        <!-- Game Holder -->
+                        <div class="flex w-full justify-between bg-card p-2 rounded-lg">
+                            <div class="flex gap-2">
+                                <img src="src/images/game-icons/hayday.jpeg" alt="" class="h-12 rounded-md aspect-square object-cover">
+                                <div>
+                                    <h1 class="font-medium">HayDay</h1>
+                                    <p class="text-sm text-paragraph">Science</p>
+                                </div>
+                            </div>
+                        </div><!-- End of Game Holder -->
+
+                        <!-- Game Holder -->
+                        <div class="flex w-full justify-between bg-card p-2 rounded-lg">
+                            <div class="flex gap-2">
+                                <img src="src/images/game-icons/worldMole.jpeg" alt="" class="h-12 rounded-md aspect-square object-cover">
+                                <div>
+                                    <h1 class="font-medium">World Mole</h1>
+                                    <p class="text-sm text-paragraph">Math</p>
+                                </div>
+                            </div>
+                        </div><!-- End of Game Holder -->
+                        
+                        
+                    </div><!--End of Game container -->
+                </div>
+
+                <!-- Games -->
+                <div class="flex flex-col gap-3">
+                    <h2 class="font-medium text-lg">Quizzes</h2>
+
+                    <!-- Default when no Quiz is added -->
+                    <div class="bg-card w-full h-30 hidden items-center justify-center rounded-lg">
+                        <h1 class="text-paragraph">No Quiz added</h1>
+                    </div>
+                    
+                    <!-- Quiz container -->
+                    <div class="w-full grid grid-cols-2 gap-2 items-center justify-center rounded-lg">
+                        <!-- Quiz Holder -->
+                        <div class="flex w-full justify-between bg-card p-2 rounded-lg">
+                            <div class="flex gap-2">
+                                <img src="src/images/game-icons/hayday.jpeg" alt="" class="h-12 rounded-md aspect-square object-cover">
+                                <div>
+                                    <h1 class="font-medium">HayDay</h1>
+                                    <p class="text-sm text-paragraph">Science</p>
+                                </div>
+                            </div>
+                        </div><!-- End of Quiz Holder -->
+                    </div><!--End of Quiz container -->
+                </div>
+
+                <div class="flex items-center gap-2 absolute w-full left-0 bottom-0 px-5 pb-5 pt-10  rounded-b-4xl bg-gradient-to-t from-white via-white to-white/50">
+                    <button class=" bg-gray-100 py-1.5 px-3 w-full rounded-xl text-heading-dark font-medium">Cancel</button>
+                    <button type="submit" class="bg-blue-button py-1.5 px-3 w-full rounded-xl text-white font-medium">Save</button>
+                </div>
+            </div>
+        </div><!-- End of Lesson View Info-->
+    </section>
+    
 </main>
