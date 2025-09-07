68504b0539178850dfde4271312539ae9d0882ba
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-09-07 09:49:16 +0800

    change in award only per month is rendered

--- app/Livewire/AwardAside.php
+++ app/Livewire/AwardAside.php
@@ -9,7 +9,12 @@ class AwardAside extends Component
 {
     public function render()
     {
-        $feeds = Feed::where('group', 'award')->orderByDesc('created_at')->limit(10)->get();
+        $feeds = Feed::where('group', 'award')
+            ->whereMonth('created_at', now()->month)
+            ->whereYear('created_at', now()->year)
+            ->orderByDesc('created_at')
+            ->limit(10)
+            ->get();
         return view('livewire.award-aside', compact('feeds'));
     }
 }
