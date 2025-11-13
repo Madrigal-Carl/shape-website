af6dffec4a73ba98a17aeca736c19442cd2499b9
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-11-14 01:34:19 +0800

    fix the url for the video in api

--- app/Http/Resources/VideoResource.php
+++ app/Http/Resources/VideoResource.php
@@ -14,11 +14,20 @@ class VideoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // Helper function to determine if URL is external
+        $formatUrl = function (?string $url): ?string {
+            if (!$url) return null;
+            if (str_starts_with($url, 'http://') || str_starts_with($url, 'https://')) {
+                return $url;
+            }
+            return asset('storage/' . $url);
+        };
+
         return [
             'id'        => $this->id,
             'lesson_id' => $this->lesson_id,
             'title'     => $this->title,
-            'url'       => $this->url ? asset('storage/' . $this->url) : null,
+            'url'       => $formatUrl($this->url),
             'thumbnail' => $this->thumbnail ? asset('storage/' . $this->thumbnail) : null,
             'created_at' => $this->created_at?->toDateTimeString(),
             'updated_at' => $this->updated_at?->toDateTimeString(),
